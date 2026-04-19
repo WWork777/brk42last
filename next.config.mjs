715420385e8d5538624f3263ed7f181756b7b63b
id0ntc4re <parsikovevgenij470@gmail.com>
2026-04-19 11:36:14 +0700

    qwe

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+// Проверяем, запущено ли приложение в продакшене
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-    images: {
-        formats: ['image/avif', 'image/webp'],
-        domains:['pic.rutube.ru', 'avatars.mds.yandex.net']
-    },
+  // Включаем префикс для всех статических файлов (JS, CSS, шрифты)
+  assetPrefix: isProd ? "https://cdn.brk42.ru" : undefined,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+    // Добавляем твой CDN в список разрешенных доменов для картинок
+    domains: ["pic.rutube.ru", "avatars.mds.yandex.net", "cdn.brk42.ru"],
+  },
 };
 
 export default nextConfig;
